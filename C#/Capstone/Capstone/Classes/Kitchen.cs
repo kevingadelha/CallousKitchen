f2fcfc3f514c7be627c5e135a836e3416b608fb0
kevingadelhasheridan <gadelha@sheridancollege.ca>
2020-04-03 20:43:29 -0400

    Added primary keys

--- C#/Capstone/Capstone/Classes/Kitchen.cs
+++ C#/Capstone/Capstone/Classes/Kitchen.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,8 @@ namespace Capstone.Classes
 {
 	public class Kitchen
 	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public virtual List<Food> Inventory { get; set; }
