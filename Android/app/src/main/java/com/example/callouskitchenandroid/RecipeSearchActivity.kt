fc9f2219786960b2fd50e4efe4afed1d8b710064
laurastewart115 <38572133+laurastewart115@users.noreply.github.com>
2020-11-02 19:53:51 -0500

    [Laura] progress on recipe search by ingredient

--- Android/app/src/main/java/com/example/callouskitchenandroid/RecipeSearchActivity.kt
+++ Android/app/src/main/java/com/example/callouskitchenandroid/RecipeSearchActivity.kt
@@ -1,8 +1,10 @@
 package com.example.callouskitchenandroid
 
+import android.app.SearchManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.SearchView
 import kotlinx.android.synthetic.main.activity_kitchen_list.bottomNav
 import kotlinx.android.synthetic.main.activity_recipe_search.*
@@ -17,20 +19,40 @@ class RecipeSearchActivity : AppCompatActivity() {
 
         val searchBar = findViewById<SearchView>(R.id.searchViewRecipes)
 
-        // temp
+        // TODO: I have to figure out some xml configurations
+        if (Intent.ACTION_SEARCH == intent.action)
+        {
+            intent.getStringExtra(SearchManager.QUERY)?.also  {
+                query -> searchRecipes(query)
+            }
+        }
+
+        suggestRecipes()
+
+        // TODO: get recipes from the service
         var recipes: ArrayList<Recipe> = arrayListOf<Recipe>()
         val recipeListAdapter = RecipeListAdapter(this, recipes)
         listViewRecipe.adapter = recipeListAdapter
 
     }
 
+    private fun searchRecipes(query: String)
+    {
+        // search based on the ingredient
+
+        // present the results in the list view
+    }
+
+    private fun suggestRecipes()
+    {
+        // suggest a few recipes based on expiring food items and use them to populate the recipe list
+    }
+
     private fun setNavigation() {
         bottomNav.setOnNavigationItemSelectedListener {
             when (it.itemId){
                 R.id.navigation_recipes -> {
-                    // go to recipes
-                    val intent = Intent(this@RecipeSearchActivity, RecipeSearchActivity::class.java)
-                    startActivity(intent)
+                    // stay in recipes
                     true
                 }
                 R.id.navigation_inventory -> {
--- Android/app/src/main/java/com/example/callouskitchenandroid/RecipeViewActivity.kt
+++ Android/app/src/main/java/com/example/callouskitchenandroid/RecipeViewActivity.kt
@@ -2,10 +2,17 @@ package com.example.callouskitchenandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class RecipeViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_view)
+
+        val recipeDisplay = findViewById<WebView>(R.id.webviewRecipe)
+
+        // example
+        recipeDisplay.loadUrl("https://www.allrecipes.com/recipe/260540/chef-johns-sourdough-bread/")
+
     }
 }
