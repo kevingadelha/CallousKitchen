c4c18f4cac01f3625eed7e1eae4a10b6aa655e3b
Peter Szadurski <44846197+PeterSzadurski@users.noreply.github.com>
2020-11-07 18:57:16 -0500

    trying to fix deployment issue

--- C#/Capstone/Capstone/Apis/ApiHelper.cs
+++ C#/Capstone/Capstone/Apis/ApiHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
@@ -13,9 +14,12 @@ namespace Capstone.Apis
 
         public static void InitializeClient()
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             ApiClient = new HttpClient();
             ApiClient.DefaultRequestHeaders.Accept.Clear();
             ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
         }
     }
 }
