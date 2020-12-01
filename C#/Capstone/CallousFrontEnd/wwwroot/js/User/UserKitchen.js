45a71a60c92b6d951abf42209c523b30ff3bb365
Peter Szadurski <44846197+PeterSzadurski@users.noreply.github.com>
2020-12-01 15:53:40 -0500

    [webApp] being unable to modify sorted foods

--- C#/Capstone/CallousFrontEnd/wwwroot/js/User/UserKitchen.js
+++ C#/Capstone/CallousFrontEnd/wwwroot/js/User/UserKitchen.js
@@ -1,104 +1,106 @@
 ﻿// Author: Peter Szadurski
 $(document).ready(function () {
     console.log("Activated");
-    $(".addFoodBtn").click(function () {
-        var KitchenId = $(this).data("kitchenId");
-        $.ajax({
-            type: 'GET',
-            url: "AddEditFoodView",
-            data: {
-                "kId": KitchenId,
-                "fId": 0
-            },
-            success: function (result) {
-                $("#AddEditFoodBody").html(result);
-            }
-
-        });
-    });
-    $(".editFoodBtn").click(function () {
-        var KitchenId = $(this).data("kitchenId");
-        var FoodId = $(this).data("foodId");
-        console.log("Edit food");
-        $.ajax({
-            type: 'GET',
-            url: "AddEditFoodView",
-            data: {
-                "kId": KitchenId,
-                "fId": FoodId
-            },
-            success: function (result) {
-                $("#AddEditFoodBody").html(result);
-            }
-
-        });
-    });
-    $(".eatFoodBtn").click(function () {
-        var FoodId = $(this).data("foodId");
-        var isVegan = $(this).data("vegan");
-        var isVeg = $(this).data("veg");
-        var userVegan = $("#isVegan").val();
-        var userVeg = $("#isVeg").val();
-
-        // hidden inputs are always strings, check string instead of bool
-
-        if ((userVeg == "True" && isVeg != 1) || (userVegan == "True" && isVegan != 1)) {
-            var com = false;
-            $.ajax({
-                type: 'GET',
-                url: "EatFoodView",
-                data: {
-                    "fId": FoodId
-                },
-                beforeSend: function () {
-
-                    com = (confirm("This food does not match your diet preferences."));
-                },
-                success: function (result) {
-                    if (com) {
-                        $('#EatFood').modal('show');
-                        $("#EatFoodBody").html(result);
-                    }
-                }
-            });
-        }
-        else {
-            $.ajax({
-                type: 'GET',
-                url: "EatFoodView",
-                data: {
-                    "fId": FoodId
-                },
-                success: function (result) {
-                    $('#EatFood').modal('show');
-                    $("#EatFoodBody").html(result);
-
-                }
-            });
-        }
-    });
-    $(".deleteFoodBtn").click(function () {
-        var FoodId = $(this).data("foodId");
-        console.log("Delete: " + FoodId);
-        $.ajax({
-            type: 'Delete',
-            url: "DeleteFood",
-            data: {
-                "fId": FoodId
-            },
-            success: function (result) {
-                $("#Kitchens").html(result);
-                console.log("Refresh");
-            }
-            ,
-            complete: function (result) {
-                console.log(result);
-            }
-
-        });
-    });
 
 });
+
+$(".addFoodBtn").on("click", function () {
+    var KitchenId = $(this).data("kitchenId");
+    $.ajax({
+        type: 'GET',
+        url: "AddEditFoodView",
+        data: {
+            "kId": KitchenId,
+            "fId": 0
+        },
+        success: function (result) {
+            $("#AddEditFoodBody").html(result);
+        }
+
+    });
+});
+$(document).on("click", ".editFoodBtn", function () {
+    var KitchenId = $(this).data("kitchenId");
+    var FoodId = $(this).data("foodId");
+    console.log("Edit food");
+    $.ajax({
+        type: 'GET',
+        url: "AddEditFoodView",
+        data: {
+            "kId": KitchenId,
+            "fId": FoodId
+        },
+        success: function (result) {
+            $("#AddEditFoodBody").html(result);
+        }
+
+    });
+});
+$(document).on("click", ".eatFoodBtn", function () {
+    var FoodId = $(this).data("foodId");
+    var isVegan = $(this).data("vegan");
+    var isVeg = $(this).data("veg");
+    var userVegan = $("#isVegan").val();
+    var userVeg = $("#isVeg").val();
+
+    // hidden inputs are always strings, check string instead of bool
+
+    if ((userVeg == "True" && isVeg != 1) || (userVegan == "True" && isVegan != 1)) {
+        var com = false;
+        $.ajax({
+            type: 'GET',
+            url: "EatFoodView",
+            data: {
+                "fId": FoodId
+            },
+            beforeSend: function () {
+
+                com = (confirm("This food does not match your diet preferences."));
+            },
+            success: function (result) {
+                if (com) {
+                    $('#EatFood').modal('show');
+                    $("#EatFoodBody").html(result);
+                }
+            }
+        });
+    }
+    else {
+        $.ajax({
+            type: 'GET',
+            url: "EatFoodView",
+            data: {
+                "fId": FoodId
+            },
+            success: function (result) {
+                $('#EatFood').modal('show');
+                $("#EatFoodBody").html(result);
+
+            }
+        });
+    }
+});
+$(document).on("click", ".deleteFoodBtn", function () {
+    var FoodId = $(this).data("foodId");
+    console.log("Delete: " + FoodId);
+    $.ajax({
+        type: 'Delete',
+        url: "DeleteFood",
+        data: {
+            "fId": FoodId
+        },
+        success: function (result) {
+            $("#Kitchens").html(result);
+            console.log("Refresh");
+        }
+        ,
+        complete: function (result) {
+            console.log(result);
+        }
+
+    });
+});
+
 
 $("#AddFood").on("click", "#btnBarcode", function () {
     var nameTb = $("#Food_Name");
@@ -155,7 +157,7 @@ $("#tbSearchbar").on("keyup", function () {
  * @param {Number} rowId
  */
 
-function SortRows(rowId) {
+function SortRows(rowId, asc) {
 
     const fridge = $("#Fridge > .foodRow");
     const cellar = $("#Cellar > .foodRow");
@@ -172,61 +174,29 @@ function SortRows(rowId) {
     const arrOther = Array.from(other);
     const arrFreezer = Array.from(freezer);
 
-    /* const sortedFridge = arrFridge.sort((x, y) => {
-         const sortx = $(x).data(rowName);
-         const sorty = $(y).data(rowName);
-         return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
-     });
-     */
+
 
     const sortedFridge = SortRow(rowId, arrFridge, dirMod);
-    /*
-    const sortedCellar = arrCeller.sort((x, y) => {
-        const sortx = $(x).data(rowName);
-        const sorty = $(y).data(rowName);
-        return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
-    });
+    const sortedCellar = SortRow(rowId, arrCeller, dirMod);
+    const sortedPantry = SortRow(rowId, arrPantry, dirMod);
+    const sortedCuboard = SortRow(rowId, arrCuboard, dirMod);
+    const sortedOther = SortRow(rowId, arrOther, dirMod);
+    const sortedFreezer = SortRow(rowId, arrFreezer, dirMod);
 
-    const sortedPantry = arrPantry.sort((x, y) => {
-        const sortx = $(x).data(rowName);
-        const sorty = $(y).data(rowName);
-        return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
-    });
-
-    const sortedCuboard = arrCuboard.sort((x, y) => {
-        const sortx = $(x).data(rowName);
-        const sorty = $(y).data(rowName);
-        return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
-    });
-
-    const sortedOther = arrOther.sort((x, y) => {
-        const sortx = $(x).data(rowName);
-        const sorty = $(y).data(rowName);
-        return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
-    });
-
-    const sortedFreezer = arrFreezer.sort((x, y) => {
-        const sortx = $(x).data(rowName);
-        const sorty = $(y).data(rowName);
-        return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
-    });
-    */
 
     fridge.parent().html(sortedFridge);
-    /*
     cellar.parent().html(sortedCellar);
     pantry.parent().html(sortedPantry);
     cuboard.parent().html(sortedCuboard);
     other.parent().html(sortedOther);
     freezer.parent().html(sortedFreezer);
-    */
+
 }
 
 function SortRow(rowId, arr, dirMod) {
     var sorted;
     switch (rowId) {
         case 0:
-            console.log("inside name");
             sorted = arr.sort((x, y) => {
                 const sortx = $(x).data("foodname");
                 const sorty = $(y).data("foodname");
@@ -235,11 +205,33 @@ function SortRow(rowId, arr, dirMod) {
             return sorted;
             break;
         case 1:
-            console.log("inside count");
-
             sorted = arr.sort((x, y) => {
                 const sortx = Number($(x).data("foodcount"));
                 const sorty = Number($(y).data("foodcount"));
+                return sortx < sorty ? (1 * dirMod) : (-1 * dirMod);
+            });
+            return sorted;
+            break;
+        case 2:
+            sorted = arr.sort((x, y) => {
+                const sortx = $(x).data("foodclassifier");
+                const sorty = $(y).data("foodclassifier");
+                return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
+            });
+            return sorted;
+            break;
+        case 3:
+            sorted = arr.sort((x, y) => {
+                const sortx = Date.parse($(x).data("fooddate"));
+                const sorty = Date.parse($(y).data("fooddate"));
+                return sortx > sorty ? (1 * dirMod) : (-1 * dirMod);
+            });
+            return sorted;
+            break;
+        case 4:
+            sorted = arr.sort((x, y) => {
+                const sortx = $(x).data("foodfav");
+                const sorty = $(y).data("foodfav");
                 return sortx < sorty ? (1 * dirMod) : (-1 * dirMod);
             });
             return sorted;
@@ -253,4 +245,12 @@ $("#btnName").on("click", function () {
 $("#btnCount").on("click", function () {
     SortRows(1);
 });
-
+$("#btnClassifier").on("click", function () {
+    SortRows(2);
+});
+$("#btnExpDate").on("click", function () {
+    SortRows(3);
+});
+$("#btnFav").on("click", function () {
+    SortRows(4);
+});
