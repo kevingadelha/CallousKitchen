f2aacb7dade0e4b7542d01c39d77c82734b1ccb0
Peter Szadurski <44846197+PeterSzadurski@users.noreply.github.com>
2020-11-30 22:42:24 -0500

    [webApp] Made shoppingList and recipe modal work on every page

---
+++ C#/Capstone/CallousFrontEnd/wwwroot/js/User/Main.js
@@ -0,0 +1,19 @@
+﻿$("#btnRecipeModal").on("click", function () {
+    console.log("recipe");
+    $.ajax({
+        type: 'Get',
+        url: "RecipeSearchView",
+        success: function (result) {
+            $("#recipeSearchBody").html(result);
+        }
+    });
+});
+$("#btnShoppingListModal").on("click", function () {
+    $.ajax({
+        type: 'Post',
+        url: "ShoppingList",
+        success: function (result) {
+            $("#shoppingListBody").html(result);
+        }
+    });
+});
--- C#/Capstone/CallousFrontEnd/wwwroot/js/User/UserKitchen.js
+++ C#/Capstone/CallousFrontEnd/wwwroot/js/User/UserKitchen.js
@@ -121,28 +121,11 @@ $("#AddFood").on("click", "#btnBarcode", function () {
     });
 
 });
-$("#btnRecipeModal").on("click", function () {
-    console.log("recipe");
-    $.ajax({
-        type: 'Get',
-        url: "RecipeSearchView",
-        success: function (result) {
-            $("#recipeSearchBody").html(result);
-        }
-    });
-});
 
 
 
-$("#btnShoppingListModal").on("click", function () {
-    $.ajax({
-        type: 'Post',
-        url: "ShoppingList",
-        success: function (result) {
-            $("#shoppingListBody").html(result);
-        }
-    });
-});
+
+
 
 $("#tbSearchbar").on("keyup", function () {
     var search = $(this).val().toLowerCase();
